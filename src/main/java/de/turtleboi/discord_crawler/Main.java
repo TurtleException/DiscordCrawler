dce40fe3a449cffd455eea72a5af84006c0da5c0
TurtleException <turtle.exception@eldritch.de>
2024-05-14 02:19:22 +0200

    Rewrite (start over)

---
+++ src/main/java/de/turtleboi/discord_crawler/Main.java
@@ -0,0 +1,35 @@
+package de.turtleboi.discord_crawler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.Properties;
+
+public class Main {
+    public static final String VERSION;
+
+    static {
+        System.out.print("Starting DiscordCrawler");
+
+        try (InputStream r = Main.class.getResourceAsStream("meta.properties")) {
+            Properties metaProperties = new Properties();
+            metaProperties.load(r);
+
+            VERSION = metaProperties.getProperty("version");
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * Called by the JVM when starting this program.
+     */
+    public static void main(String[] args) {
+        /* VERSION*/
+        if (VERSION == null)
+            throw new Error("Version may not be null. This application was not built properly.");
+        System.out.printf(" version %s...%n", VERSION);
+
+        // TODO
+    }
+}
