5c26725e2c64c8474ef5081eeb20f4de3767ad3b
TurtleException <turtle.exception@eldritch.de>
2024-05-15 22:56:16 +0200

    Implement nested jobs

--- src/main/java/de/turtleboi/discord_crawler/job/CompleteJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/CompleteJob.java
@@ -3,11 +3,17 @@ package de.turtleboi.discord_crawler.job;
 import de.turtleboi.discord_crawler.collection.Collector;
 import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import org.jetbrains.annotations.NotNull;
 
 public record CompleteJob() implements Job {
     @Override
     public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
-        // TODO
+        for (Guild guild : jda.getGuilds())
+            executor.queueJob(new GuildJob(guild.getIdLong()));
+
+        for (Channel channel : jda.getPrivateChannels())
+            executor.queueJob(new ChannelJob(channel.getIdLong()));
     }
 }
--- src/main/java/de/turtleboi/discord_crawler/job/GuildJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/GuildJob.java
@@ -3,11 +3,21 @@ package de.turtleboi.discord_crawler.job;
 import de.turtleboi.discord_crawler.collection.Collector;
 import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import org.jetbrains.annotations.NotNull;
 
 public record GuildJob(long guild) implements Job {
     @Override
     public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
-        // TODO
+        Guild guild = jda.getGuildById(this.guild);
+
+        if (guild == null)
+            throw new NullPointerException("No such guild: " + this.guild);
+
+        for (Channel channel : guild.getChannels())
+            executor.queueJob(new ChannelJob(channel.getIdLong()));
+
+        // TODO: metadata
     }
 }
