7ba09f6da25ad99413947f52e2bd3acf88b3ffa9
TurtleException <turtle.exception@eldritch.de>
2024-05-19 01:50:05 +0200

    Retrieve threads from IThreadContainer channels

--- src/main/java/de/turtleboi/discord_crawler/job/ChannelJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/ChannelJob.java
@@ -3,6 +3,8 @@ package de.turtleboi.discord_crawler.job;
 import de.turtleboi.discord_crawler.collection.Collector;
 import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.attribute.IThreadContainer;
 import net.dv8tion.jda.api.requests.Route;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,12 +18,18 @@ public record ChannelJob(
 
     @Override
     public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
+        Channel channel = jda.getChannelById(Channel.class, this.channel);
+
+        if (channel == null)
+            throw new NullPointerException("No such channel: " + this.channel);
+
         String channelId = String.valueOf(this.channel);
 
         executor.queueJob(new DataObjectJob(collectorPath + "/channel", Route.Channels.GET_CHANNEL.compile(channelId)));
 
-        if (isGuild())
-            executor.queueJob(new DataArrayJob(collectorPath + "/permissions", Route.Channels.GET_PERMISSIONS.compile(channelId)));
+        if (channel instanceof IThreadContainer threadContainer) {
+            executor.queueJob(new DataObjectJob(collectorPath + "/threads", Route.Channels.LIST_PUBLIC_ARCHIVED_THREADS.compile(channelId)));
+        }
     }
 
     public boolean isGuild() {
