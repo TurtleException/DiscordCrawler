6e0853972238b1aca90c29d17b456de1caab3909
TurtleException <turtle.exception@eldritch.de>
2024-05-19 00:42:51 +0200

    Implement metadata collection

--- src/main/java/de/turtleboi/discord_crawler/job/ChannelJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/ChannelJob.java
@@ -3,6 +3,7 @@ package de.turtleboi.discord_crawler.job;
 import de.turtleboi.discord_crawler.collection.Collector;
 import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.requests.Route;
 import org.jetbrains.annotations.NotNull;
 
 public record ChannelJob(
@@ -15,6 +16,15 @@ public record ChannelJob(
 
     @Override
     public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
-        // TODO
+        String channelId = String.valueOf(this.channel);
+
+        executor.queueJob(new DataObjectJob(collectorPath + "/channel", Route.Channels.GET_CHANNEL.compile(channelId)));
+
+        if (isGuild())
+            executor.queueJob(new DataArrayJob(collectorPath + "/permissions", Route.Channels.GET_PERMISSIONS.compile(channelId)));
+    }
+
+    public boolean isGuild() {
+        return this.collectorPath.startsWith("guilds");
     }
 }
--- src/main/java/de/turtleboi/discord_crawler/job/GuildJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/GuildJob.java
@@ -5,6 +5,7 @@ import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.requests.Route;
 import org.jetbrains.annotations.NotNull;
 
 public record GuildJob(
@@ -18,6 +19,20 @@ public record GuildJob(
             throw new NullPointerException("No such guild: " + this.guild);
 
         for (Channel channel : guild.getChannels())
-            executor.queueJob(new ChannelJob("guilds/" + guild + "/channels/" + channel.getId(), channel.getIdLong()));
+            executor.queueJob(new ChannelJob("guilds/" + this.guild + "/channels/" + channel.getId(), channel.getIdLong()));
+
+        String guildId = String.valueOf(this.guild);
+
+        executor.queueJob(new DataObjectJob("guilds/" + guildId + "/guild"         , Route.Guilds.GET_GUILD.compile(guildId)));
+        executor.queueJob(new DataObjectJob("guilds/" + guildId + "/audit_logs"    , Route.Guilds.GET_AUDIT_LOGS.compile(guildId)));
+        executor.queueJob(new DataObjectJob("guilds/" + guildId + "/guild_embed"   , Route.Guilds.GET_GUILD_EMBED.compile(guildId)));
+        executor.queueJob(new DataObjectJob("guilds/" + guildId + "/welcome_screen", Route.Guilds.GET_WELCOME_SCREEN.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/channels"     , Route.Guilds.GET_CHANNELS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/bans"         , Route.Guilds.GET_BANS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/webhooks"     , Route.Guilds.GET_WEBHOOKS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/emojis"       , Route.Guilds.GET_GUILD_EMOJIS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/voice_regions", Route.Guilds.GET_VOICE_REGIONS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/integrations" , Route.Guilds.GET_INTEGRATIONS.compile(guildId)));
+        executor.queueJob(new DataArrayJob("guilds/" + guildId + "/events"       , Route.Guilds.GET_SCHEDULED_EVENTS.compile(guildId)));
     }
 }
