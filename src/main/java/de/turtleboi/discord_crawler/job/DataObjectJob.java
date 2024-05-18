b88465d789eacd74a951a0b1528dfc928c219be7
TurtleException <turtle.exception@eldritch.de>
2024-05-19 00:42:59 +0200

    Ignore 404 errors

--- src/main/java/de/turtleboi/discord_crawler/job/DataObjectJob.java
+++ src/main/java/de/turtleboi/discord_crawler/job/DataObjectJob.java
@@ -3,6 +3,7 @@ package de.turtleboi.discord_crawler.job;
 import de.turtleboi.discord_crawler.collection.Collector;
 import de.turtleboi.discord_crawler.executor.Executor;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 import net.dv8tion.jda.api.requests.Route;
 import net.dv8tion.jda.internal.requests.RestActionImpl;
 import org.jetbrains.annotations.NotNull;
@@ -15,7 +16,17 @@ public record DataObjectJob(
     public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
         RestActionImpl<String> restAction = new RestActionImpl<>(jda, route, (response, request) -> response.getObject().toPrettyString());
 
-        String data = restAction.complete();
+        String data;
+        try {
+            data = restAction.complete();
+        } catch (ErrorResponseException e) {
+            if (e.getResponse().code == 404) {
+                // ignore
+                return;
+            } else {
+                throw e;
+            }
+        }
 
         collector.write(collectorPath + ".json", data, false);
     }
