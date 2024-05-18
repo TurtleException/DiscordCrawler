3c44374429afec71a872f8313879c3f979073f90
TurtleException <turtle.exception@eldritch.de>
2024-05-18 23:34:59 +0200

    Create DataObjectJob & DataArrayJob

---
+++ src/main/java/de/turtleboi/discord_crawler/job/DataObjectJob.java
@@ -0,0 +1,22 @@
+package de.turtleboi.discord_crawler.job;
+
+import de.turtleboi.discord_crawler.collection.Collector;
+import de.turtleboi.discord_crawler.executor.Executor;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.requests.Route;
+import net.dv8tion.jda.internal.requests.RestActionImpl;
+import org.jetbrains.annotations.NotNull;
+
+public record DataObjectJob(
+        @NotNull String collectorPath,
+        @NotNull Route.CompiledRoute route
+) implements Job {
+    @Override
+    public void execute(@NotNull JDA jda, @NotNull Executor executor, @NotNull Collector collector) {
+        RestActionImpl<String> restAction = new RestActionImpl<>(jda, route, (response, request) -> response.getObject().toPrettyString());
+
+        String data = restAction.complete();
+
+        collector.write(collectorPath + ".json", data, false);
+    }
+}
