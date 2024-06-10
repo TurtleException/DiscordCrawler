d2e44971dc8e26395d4be304132b0b4a7209d2cd
TurtleException <turtle.exception@eldritch.de>
2024-06-11 00:12:45 +0200

    Implement capability to write arrays buffered

--- src/main/java/de/turtleboi/discord_crawler/collection/Collector.java
+++ src/main/java/de/turtleboi/discord_crawler/collection/Collector.java
@@ -1,13 +1,25 @@
 package de.turtleboi.discord_crawler.collection;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Collector {
+    private static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .serializeNulls()
+            .create();
+
     private final File outputDir;
+
+    private final ConcurrentHashMap<String, JsonArray> arrayBuffer = new ConcurrentHashMap<>();
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public Collector(@NotNull File outputDir) {
@@ -25,5 +37,22 @@ public class Collector {
         } catch (IOException e) {
             // TODO: handle
         }
+    }
+
+    public synchronized void writeToArray(@NotNull String path, @NotNull JsonElement... data) {
+        JsonArray arr = this.arrayBuffer.computeIfAbsent(path, s -> new JsonArray());
+
+        for (JsonElement element : data)
+            arr.add(element);
+
+        this.arrayBuffer.put(path, arr);
+    }
+
+    public void flushArray(@NotNull String path) {
+        JsonArray arr = arrayBuffer.remove(path);
+
+        if (arr == null) return;
+
+        this.write(path, GSON.toJson(arr), false);
     }
 }
